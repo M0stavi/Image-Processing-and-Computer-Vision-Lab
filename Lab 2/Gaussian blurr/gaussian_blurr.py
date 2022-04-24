1329d6fd3cb1d291181afd77c528c6aa5ddf5461
M0stavi <mostavi3673@gmail.com>
2022-04-24 16:34:01 +0600

    Gaussian blurr

---
+++ Lab 2/Gaussian blurr/gaussian_blurr.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 24 15:44:13 2022
+
+@author: Asus
+"""
+
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+path = "F:/Online Class/4-1/zLabs/Vision/rubiks_cube.png"
+
+img = cv.imread(path)
+
+img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+plt.imshow(img, 'gray')
+
+plt.title("Input")
+
+plt.show()
+
+print("Enter kernel height: ")
+
+k_h = int(input())
+
+print("Enter kernel weidth: ")
+
+k_w = int(input())
+
+kernel = np.zeros((k_h,k_w), np.float32)
+
+sigma = 1.0
+
+s = 2.0*sigma*sigma
+
+pi = 3.1416
+
+a = kernel.shape[0] // 2
+b = kernel.shape[1] // 2
+
+for i in range(-a, a+1):
+    for j in range(-b,b+1):
+        r = math.sqrt(i*i+j*j)
+        r/=s
+        r = math.exp(-(r))
+        r/=(pi*s)
+        kernel[i+a][j+b] = r
+        
+m = img.shape[0]
+n = img.shape[1]
+op = np.zeros((m,n), np.float32)
+ksum = kernel.sum()
+
+for i in range(m):
+    for j in range(n):
+        val = 0
+        for x in range(-a,a+1):
+            for y in range(-b,b+1):
+                if i-x>=0 and i-x<m and j-y>=0 and j-y<n:
+                    val+= kernel[x+a][y+b]*img[i-x][j-y]
+                else:
+                    val+=0
+        val/=ksum
+        op[i][j] = val
+
+plt.imshow(op,'gray')
+plt.title("Output for gaussian blurr")
+plt.show()
