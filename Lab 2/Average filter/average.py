75e5d7020aae37e425252800f97057abeb5996ca
M0stavi <mostavi3673@gmail.com>
2022-04-24 22:06:27 +0600

    Mean filtering

--- Lab 2/Average filter/average.py
+++ Lab 2/Average filter/average.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr 24 20:27:41 2022
+Created on Sun Apr 24 21:42:13 2022
 
 @author: Asus
 """
@@ -12,13 +12,13 @@ import math
 
 path = "C:/Users/Asus/imagelab/Image-Processing-and-Computer-Vision-Lab/Lab 2/slide.png"
 
-img  = cv.imread(path)
+img = cv.imread(path)
 
 img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 plt.imshow(img, "gray")
 
-plt.title("Input for median filter:")
+plt.title("Input: ")
 
 plt.show()
 
@@ -26,41 +26,33 @@ print("Enter kernel height: ")
 
 k_h = int(input())
 
-print("Enter kernel width: ")
+print("Enter kernel weidth: ")
 
 k_w = int(input())
 
-kernel = np.ones((k_h,k_w), np.float32) #box kernel
+kernel = np.ones((k_h,k_w), np.float32)
 
 a = kernel.shape[0] // 2
 b = kernel.shape[1] // 2
 m = img.shape[0]
 n = img.shape[1]
-total = k_h*k_w
 
 op = np.zeros((m,n), np.float32)
 
+ksum = kernel.sum()
+
 for i in range(m):
     for j in range(n):
-        values = []
+        value = 0
         for x in range(-a,a+1):
             for y in range(-b,b+1):
-                if i+x>=0 and i+x<m and j+y>=0 and j+y<n:
-                    values.append(kernel[a+x][b+y]*img[i+x][j+y])
+                if i+x >= 0 and i+x <m and j+y>=0 and j+y<n:
+                    value+=kernel[a+x][b+y]*img[i+x][j+y]
                 else:
-                    values.append(0)
-        values.sort()
-        median = len(values) // 2
-        op[i][j] = values[median]
-        op[i][j]/=total
-        
+                    value+=0
+        value/=ksum
+        op[i][j] = value
+
 plt.imshow(op, "gray")
-
-plt.title("Output for median: ")
-
+plt.title("Output for mean filter: ")
 plt.show()
-
-
-                
-        
-
