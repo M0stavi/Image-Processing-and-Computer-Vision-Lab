83241ea421af0e24b2ae5d6838da0a450c12368f
M0stavi <mostavi3673@gmail.com>
2022-06-28 13:02:12 +0600

    notch

--- Labtest practice/fourier_sec_B/notch.py
+++ Labtest practice/fourier_sec_B/notch.py
@@ -5,3 +5,158 @@ Created on Tue Jun 28 01:24:44 2022
 @author: Asus
 """
 
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+xx= []
+yy=[]
+
+def click_event(event, x, y, flags, params):
+ 
+    # checking for left mouse clicks
+    if event == cv.EVENT_LBUTTONDOWN:
+ 
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+        xx.append(x)
+        yy.append(y)
+        # print(uk)
+ 
+        # displaying the coordinates
+        # on the image window
+        font = cv.FONT_HERSHEY_SIMPLEX
+        cv.putText(img, str(x) + ',' +
+                    str(y), (x,y), font,
+                    1, (255, 0, 0), 2)
+        cv.imshow('image', img)
+ 
+    # checking for right mouse clicks    
+    if event==cv.EVENT_RBUTTONDOWN:
+ 
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+        
+ 
+        # displaying the coordinates
+        # on the image window
+        font = cv.FONT_HERSHEY_SIMPLEX
+        b = img[y, x, 0]
+        g = img[y, x, 1]
+        r = img[y, x, 2]
+        cv.putText(img, str(b) + ',' +
+                    str(g) + ',' + str(r),
+                    (x,y), font, 1,
+                    (255, 255, 0), 2)
+        cv.imshow('image', img)
+        
+path = 'pi.jpg'
+img = cv.imread(path)
+
+
+img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+plt.imshow(img,'gray')
+
+plt.show()
+
+
+f = np.fft.fft2(img)
+
+sft = np.fft.fftshift(f)
+
+mag = np.abs(sft)
+
+mag=np.log(mag)
+
+plt.imshow(mag,'gray')
+
+plt.show()
+
+k=0
+
+while 1:
+    if k==2:
+        break
+    mag = np.log(mag)
+    k+=1
+    
+cv.imshow('image',mag)
+    
+
+cv.setMouseCallback('image',click_event)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+cv.imshow('image',mag)
+    
+
+cv.setMouseCallback('image',click_event)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+cv.imshow('image',mag)
+    
+
+cv.setMouseCallback('image',click_event)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+cv.imshow('image',mag)
+    
+
+cv.setMouseCallback('image',click_event)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+mm = img.shape[0]//2
+nn = img.shape[1]//2
+
+notch = np.zeros((img.shape[0],img.shape[1]),np.float32)
+
+d0=50
+n=2
+
+for u in range(img.shape[0]):
+    for v in range(img.shape[1]):
+        for k in range(4):
+            dk = np.sqrt((u-mm-xx[k])**2+(v-nn-yy[k])**2)
+            dkk = np.sqrt((u-mm+xx[k])**2+(v-nn+yy[k])**2)
+            if dk>d0 or dkk>d0:
+                if dk==0:
+                    dk=1
+                if dkk == 0:
+                    dkk = 1
+                notch[u][v] += (1/(1+(d0/dk)**(2*n)))+(1/(1+(d0/dkk)**(2*n)))
+
+ni = np.fft.fftshift(notch)
+plt.imshow(ni,'gray')
+plt.show()
+plt.imshow(notch,'gray')
+plt.show()
+
+mag = np.abs(sft)
+
+phase=np.angle(sft)
+
+mag=mag*ni
+
+op = np.multiply(mag,np.exp(1j*phase))
+
+op=np.fft.ifftshift(op)
+
+op = np.real(np.fft.ifft2(op))
+
+plt.imshow(op,'gray')
+plt.show()
--- Labtest practice/randoms.py
+++ Labtest practice/randoms.py
@@ -10,9 +10,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-path = 'homo.jpg'
+xx= []
+yy=[]
 
+def click_event(event, x, y, flags, params):
+ 
+    # checking for left mouse clicks
+    if event == cv.EVENT_LBUTTONDOWN:
+ 
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+        xx.append(x)
+        yy.append(y)
+        # print(uk)
+ 
+        # displaying the coordinates
+        # on the image window
+        font = cv.FONT_HERSHEY_SIMPLEX
+        cv.putText(img, str(x) + ',' +
+                    str(y), (x,y), font,
+                    1, (255, 0, 0), 2)
+        cv.imshow('image', img)
+ 
+    # checking for right mouse clicks    
+    if event==cv.EVENT_RBUTTONDOWN:
+ 
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+        
+ 
+        # displaying the coordinates
+        # on the image window
+        font = cv.FONT_HERSHEY_SIMPLEX
+        b = img[y, x, 0]
+        g = img[y, x, 1]
+        r = img[y, x, 2]
+        cv.putText(img, str(b) + ',' +
+                    str(g) + ',' + str(r),
+                    (x,y), font, 1,
+                    (255, 255, 0), 2)
+        cv.imshow('image', img)
+        
+path = 'pi.jpg'
 img = cv.imread(path)
+
 
 img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
@@ -20,45 +63,102 @@ plt.imshow(img,'gray')
 
 plt.show()
 
-mm = img.shape[0]
-
-nn = img.shape[1]
-
-h = np.zeros((mm,nn),np.float32)
-
-d0=50
-c=0.1
-gh=1.2
-gl=0.5
-
-for i in range(mm):
-    for j in range(nn):
-        x = (i-mm//2)**2
-        y = (j-nn//2)**2
-        d = x+y
-        r = (gh-gl)*(1-np.exp(-(c*(d/d0**2))))+gl
-        h[i][j]= r
-        
-plt.imshow(h,'gray')
-
-plt.show()
 
 f = np.fft.fft2(img)
 
-sft=np.fft.fftshift(f)
+sft = np.fft.fftshift(f)
 
 mag = np.abs(sft)
 
-phase = np.angle(sft)
+mag=np.log(mag)
 
-mag = mag*h
+plt.imshow(mag,'gray')
 
-op=np.multiply(mag,np.exp(1j*phase))
+plt.show()
 
-op= np.fft.ifftshift(op)
+k=0
+
+while 1:
+    if k==2:
+        break
+    mag = np.log(mag)
+    k+=1
+    
+cv.imshow('image',mag)
+    
+
+cv.setMouseCallback('image',click_event)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+cv.imshow('image',mag)
+    
+
+cv.setMouseCallback('image',click_event)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+cv.imshow('image',mag)
+    
+
+cv.setMouseCallback('image',click_event)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+cv.imshow('image',mag)
+    
+
+cv.setMouseCallback('image',click_event)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+mm = img.shape[0]//2
+nn = img.shape[1]//2
+
+notch = np.zeros((img.shape[0],img.shape[1]),np.float32)
+
+d0=50
+n=2
+
+for u in range(img.shape[0]):
+    for v in range(img.shape[1]):
+        for k in range(4):
+            dk = np.sqrt((u-mm-xx[k])**2+(v-nn-yy[k])**2)
+            dkk = np.sqrt((u-mm+xx[k])**2+(v-nn+yy[k])**2)
+            if dk>d0 or dkk>d0:
+                if dk==0:
+                    dk=1
+                if dkk == 0:
+                    dkk = 1
+                notch[u][v] += (1/(1+(d0/dk)**(2*n)))+(1/(1+(d0/dkk)**(2*n)))
+
+ni = np.fft.fftshift(notch)
+plt.imshow(ni,'gray')
+plt.show()
+plt.imshow(notch,'gray')
+plt.show()
+
+mag = np.abs(sft)
+
+phase=np.angle(sft)
+
+mag=mag*ni
+
+op = np.multiply(mag,np.exp(1j*phase))
+
+op=np.fft.ifftshift(op)
 
 op = np.real(np.fft.ifft2(op))
 
 plt.imshow(op,'gray')
-
 plt.show()
+
+
